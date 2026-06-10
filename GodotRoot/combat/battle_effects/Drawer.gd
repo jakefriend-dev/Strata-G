fd5b8aa1c5abdeeacaac9a4171ba14a9eefc169a
jakefriend <68086020+jakefriend-dev@users.noreply.github.com>
2026-06-10 01:33:23 -0400

    Update Drawer.gd

--- GodotRoot/combat/battle_effects/Drawer.gd
+++ GodotRoot/combat/battle_effects/Drawer.gd
@@ -3,6 +3,15 @@ extends Node2D
 var APD: ActionPreviewData # We never WIPE from this script though, since it might be linked
 var drawing: bool = false
 
+var prio_order: Array = [
+	ActionPreviewData.ROWS.PASS,
+	ActionPreviewData.ROWS.ERROR,
+	ActionPreviewData.ROWS.FALLBACK,
+	ActionPreviewData.ROWS.NEUTRAL,
+	ActionPreviewData.ROWS.BAD,
+	ActionPreviewData.ROWS.GOOD
+]
+
 func _ready():
 	batman.connect("new_action_preview_data_readied", self, "draw_action_preview")
 #	batman.connect("action_step_complete", self, "begin_drawing")
@@ -37,10 +46,11 @@ func draw_action_preview(new_APD: ActionPreviewData):
 func _draw():
 	if !drawing: return
 	if APD == null: return
-	draw_all_arrows()
+	draw_all_arrows(true)
+	draw_all_arrows(false)
 	pass
 
-func draw_all_arrows():
+func draw_all_arrows(is_outline: bool):
 #	var arrowsets: Array = [] # We want to add a series of [Rect2, Color] sets!
 	
 	# First, GATHER all the data!
@@ -48,28 +58,41 @@ func draw_all_arrows():
 	for row in APD.ROWS.size():
 		y += 1 # 0-based
 		
-		var arrow_array: Array = APD.sets.get_cell(APD.COLS.ARROW_ARRAY, y)
-		var col: Color = APD.colors[y]
+		var index: int = prio_order[y]
+		var arrow_array: Array = APD.sets.get_cell(APD.COLS.ARROW_ARRAY, index)
+		var col: Color = APD.colors[index]
+		var width: float = 4.0
+		if index == APD.ROWS.PASS or index == APD.ROWS.ERROR:
+			width = 2.0
+			if is_outline:
+				continue
+		if is_outline:
+			width += 4.0
+			col = Color.black
+			col.a = 0.5
 		
 		for arrow_rect in arrow_array: if arrow_rect is Rect2:
 			var start: Vector2 = arrow_rect.position
 			var end: Vector2 = arrow_rect.size
-			draw_arrow(start, end, col, y)
+			if is_outline:
+				var adj: float = 2.0
+				start += Vector2(adj, adj)
+				end += Vector2(adj, adj)
+			draw_arrow(start, end, col, index, width)
 	pass
 
 func draw_arrow(
 	start: Vector2,
 	end: Vector2,
 	color: Color,
-	y: int,
-#	width: float = -1,
-	filled: bool = false,
-	head_length: float = 0.25,
+	index: int,
+	width: float,
+	head_length: float = 0.15,
 	head_angle: float = PI / 4.0
 	):
-		var width: float = 4.0
-		if y == APD.ROWS.PASS:
-			width = 2.0
+		# Get the details in order for the initial line!
+		
+		
 		
 		var dir: Vector2 = end - start
 		dir = dir.normalized()
@@ -77,12 +100,23 @@ func draw_arrow(
 		var adjust: float = 0.25
 		
 		start = start + (cell_len * adjust)
-		if y == APD.ROWS.PASS:
+		if index == APD.ROWS.PASS:
 			end = end + (cell_len * adjust)
-		elif y == APD.ROWS.ERROR:
+		elif index == APD.ROWS.ERROR:
 			end = end - (cell_len * adjust)
 		
-		draw_line(start, end, color, width)
+		# Main line
+		draw_line(start, end, color, width, true)
+		
+		# Now we prep the head!
+		var head_len: Vector2 = cell_len * head_length
+		var head_center = end + (head_len * 0.5)
+		var head_right = end - head_len.rotated(head_angle)
+		var head_left = end - head_len.rotated(-head_angle)
+		draw_line(head_center, head_left, color, width, true)
+		draw_line(head_center, head_right, color, width, true)
+#		draw_polyline([head_right, head_center, head_left], color, width)
+#		draw_colored_polygon([head_right, head_center, head_left], color)
 		
 #		target -= origin * 2
 #		var head: Vector2 = -target.normalized() * head_length
