865cd96958458afdfae9e4ebeceddd579f9bc518
jakefriend <68086020+jakefriend-dev@users.noreply.github.com>
2026-06-23 23:47:03 -0400

    Prepping more PipSet work

---
+++ combat/battle_ui/PipSet.gd
@@ -0,0 +1,22 @@
+extends PanelContainer
+# Change this once we know what the top node should be
+
+
+
+
+#
+# AP PIPS:
+#	- LEFT: Mix 6, and +4 per pip
+#	- RIGHT: Ditto left
+#	- SPRITE: Match Center
+#
+# HEALTH PIPS:
+#	- Left/Right Ditto AP
+#	- SPRITE: Panel's position +1x
+#
+# SHIELD: Totally ditto Health
+#
+#
+#
+#
+#
