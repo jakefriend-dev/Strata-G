d980c734744391af01ed44522fb5c507a6b54f25
jakefriend <68086020+jakefriend-dev@users.noreply.github.com>
2026-06-24 01:43:24 -0400

    Working on PipSet

--- combat/battle_ui/PipSet.gd
+++ combat/battle_ui/PipSet.gd
@@ -1,18 +1,24 @@
-extends PanelContainer
+extends HBoxContainer
 # Change this once we know what the top node should be
 
+enum the_types {TBD, ACTION_POINTS, HEALTH, SHIELD}
+export (the_types) var type: int
 
+var spacing_min_left: float = 6.0
+var spacing_interval_left: float = 4.0
+var spacing_min_right: float = 6.0
+var spacing_interval_right: float = 4.0
 
+export var max_units:  int = 0
+export var curr_units: int = 0
 
 #
 # AP PIPS:
 #	- LEFT: Mix 6, and +4 per pip
 #	- RIGHT: Ditto left
-#	- SPRITE: Match Center
 #
 # HEALTH PIPS:
 #	- Left/Right Ditto AP
-#	- SPRITE: Panel's position +1x
 #
 # SHIELD: Totally ditto Health
 #
