80042fa4c96ad450255bdc45690c5c9e9f3414df
jakefriend <68086020+jakefriend-dev@users.noreply.github.com>
2026-07-13 09:23:21 -0400

    Mage now has Dismiss instead of Vine Yank

--- combat/move_actions/MS_dismiss.gd
+++ combat/move_actions/MS_dismiss.gd
@@ -1,44 +1,43 @@
 extends MoveAction
 
-var DIST: int = 3
+#var DIST: int = 3
 
-func LOAD_VARIANTS():
-	# Because we're checking BEHIND us, check one tile to the front - we only really care about if a row exists above or below us, not any particular cell position.
-	for vec in plausible_variants:
-		if batman.grid_actors.has_cellv(actor.coord + vec + actor.my_facing):
-			actualized_variants.append(vec)
-	pass
+#func LOAD_VARIANTS():
+#	# Because we're checking BEHIND us, check one tile to the front - we only really care about if a row exists above or below us, not any particular cell position.
+#	for vec in plausible_variants:
+#		if batman.grid_actors.has_cellv(actor.coord + vec + actor.my_facing):
+#			actualized_variants.append(vec)
+#	pass
 
 func PREVIEW():
-	var unoccupieds: Array = support.list_all_unoccupied_tiles_in_dir(actor.coord, actor.my_facing, DIST)
+	var unoccupieds: Array = support.list_all_unoccupied_tiles_in_dir(actor.coord, actor.my_facing)
 	if !unoccupieds.empty():
 		add_arrow(actor.coord, unoccupieds.back(), ROWS.PASS)
 	
-	var victim: Actor = support.find_nearest_actor_in_dir(actor.coord, actor.my_facing, DIST)
+	var victim: Actor = support.find_nearest_actor_in_dir(actor.coord, actor.my_facing)
 	if !utils.actorpass(victim):
-		error_text = "No target in range"
+		error_text = "No target in line of sight"
 		return
 	
-	# If victim is within 3 tiles...
+	# An actor exists!
 	
-	add_actor(victim, ROWS.NEUTRAL)
+	if strife.is_unmovable(victim):
+		add_actor(victim, ROWS.ERROR)
+		error_text = "Target is unmovable"
+		return
 	
 	var check_vector: Vector2 = batman.loaded_variant
 	var check_coord: Vector2 = victim.coord + check_vector
 	
 	if !support.is_tile_traversable_exact(victim, check_coord):
+		add_actor(victim, ROWS.ERROR)
 		add_arrow(victim.coord, check_coord, ROWS.ERROR)
 #		print("Yank preview fail; victim can't traverse destination tile")
-		error_text = "Target can't be pulled to dest"
-		return
-	
-	if strife.is_unmovable(victim):
-		add_arrow(victim.coord, check_coord, ROWS.ERROR)
-#		print("Yank preview fail; victim is not affected by force")
-		error_text = "Target is unmovable"
+		error_text = "Target can't be moved there"
 		return
 	
 	# Success case!
+	add_actor(victim, ROWS.NEUTRAL)
 	add_arrow(victim.coord, check_coord, ROWS.NEUTRAL)
 #	print("successsss")
 	passfail = true
