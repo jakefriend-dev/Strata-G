f1e230063a266b3bd91e821ab4c812877249b1b2
jakefriend <68086020+jakefriend-dev@users.noreply.github.com>
2026-07-18 16:46:08 -0400

    Update MS_press_forward.gd

--- combat/move_actions/common/MS_press_forward.gd
+++ combat/move_actions/common/MS_press_forward.gd
@@ -11,9 +11,35 @@ extends MoveAction
 
 
 func PREVIEW():
+	var my_frontline_x: int
+	var their_frontline_x: int
+	
+	if actor.faction == batman.factions.PLAYER:
+		my_frontline_x = batman.player_frontline_col
+		their_frontline_x = batman.enemy_frontline_col
+	elif actor.faction == batman.factions.ENEMY:
+		my_frontline_x = batman.enemy_frontline_col
+		their_frontline_x = batman.player_frontline_col
+	
 	# If I'm not against the frontline, fail
+	if actor.coord.x != my_frontline_x:
+		error_text = "Unit must be against frontline!"
+		add_actor(actor, ROWS.ERROR)
 	
 	# If enemies are against the frontline, fail
+	var y: int = 0 # 1-based
+	var opposing_pass: bool = true
+	for n in batman.field.board_size.y:
+		y += 1
+		var coord: Vector2 = Vector2(their_frontline_x, y)
+		var o_actor: Actor = batman.grid_actors.get_cellv(coord)
+		if utils.actorpass(o_actor):
+			if o_actor.faction != batman.factions.NEUTRAL:
+				error_text = "Opposing frontline must be clear of units!"
+				add_actor(o_actor, ROWS.ERROR)
+	
+	if error_text != "":
+		return
 	
 	passfail = true
 	pass
